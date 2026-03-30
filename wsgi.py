51fe09319ec51081cde67517a35f859f200c441c
Jetso <jetso@example.com>
2026-03-30 19:46:46 -0400

    Move app import inside try-except in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
 import sys
 import traceback
-from app import create_app
 
 try:
+    from app import create_app
     app = create_app()
     application = app
 except Exception as e:
-    print("CRITICAL: Failed to create app", file=sys.stderr)
+    print("CRITICAL: Failed to create app at import or call time", file=sys.stderr)
     traceback.print_exc()
     raise e
