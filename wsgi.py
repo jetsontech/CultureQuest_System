89b30cb900ac1cd7a0c9b091de21389b4258c5b4
Jetso <jetso@example.com>
2026-03-30 19:44:51 -0400

    Add traceback logging to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,11 @@
-from app import create_app`n`napp = create_app()`napplication = app
+import sys
+import traceback
+from app import create_app
+
+try:
+    app = create_app()
+    application = app
+except Exception as e:
+    print("CRITICAL: Failed to create app", file=sys.stderr)
+    traceback.print_exc()
+    raise e
